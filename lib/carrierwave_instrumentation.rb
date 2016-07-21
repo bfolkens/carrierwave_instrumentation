a4fb16f4b66eefb0b1457479dbf0d74eeb305632
Bradford Folkens <bfolkens@gmail.com>
2016-07-21 16:11:03 -0700

    Fixed some things, better payload

--- lib/carrierwave_instrumentation.rb
+++ lib/carrierwave_instrumentation.rb
@@ -5,8 +5,11 @@ module CarrierWaveInstrumentation
   ProcessEventName = 'process.carrierwave'.freeze
 
   def process!(*args, &block)
-    ActiveSupport::Notifications.instrument(ProcessEventName) do
-      super
+    sanitized_file = args[0]
+
+    payload = { filename: sanitized_file.filename, original_filename: sanitized_file.original_filename, content_type: sanitized_file.content_type, size: sanitized_file.size }
+    ActiveSupport::Notifications.instrument(ProcessEventName, payload) do
+      super *args, &block
     end
   end
 
