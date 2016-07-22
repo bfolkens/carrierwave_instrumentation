aa9ad6aa3fcc2e6fa9ce697d7a97ad203fa31381
Bradford Folkens <bfolkens@gmail.com>
2016-07-21 17:28:23 -0700

    lots more measures and payload data

--- lib/carrierwave_instrumentation.rb
+++ lib/carrierwave_instrumentation.rb
@@ -3,12 +3,45 @@ require 'active_support'
 
 module CarrierWaveInstrumentation
   ProcessEventName = 'process.carrierwave'.freeze
+  StoreEventName = 'store.carrierwave'.freeze
+  DownloadEventName = 'download.carrierwave'.freeze
 
   def process!(*args, &block)
     sanitized_file = args[0]
 
-    payload = { filename: sanitized_file.filename, original_filename: sanitized_file.original_filename, content_type: sanitized_file.content_type, size: sanitized_file.size }
+    payload = {
+      filename: sanitized_file.filename,
+      original_filename: sanitized_file.original_filename,
+      content_type: sanitized_file.content_type,
+      size: sanitized_file.size,
+      version: version_name || 'default',
+      processors: self.class.processors.map {|processor| processor[0] }
+    }
     ActiveSupport::Notifications.instrument(ProcessEventName, payload) do
+      super *args, &block
+    end
+  end
+
+  def store!(*args, &block)
+    payload = {
+      filename: filename,
+      original_filename: original_filename,
+      content_type: sanitized_file.content_type,
+      size: sanitized_file.size,
+      version: version_name || 'default'
+    }
+    ActiveSupport::Notifications.instrument(StoreEventName, payload) do
+      super *args, &block
+    end
+  end
+
+  def download!(*args, &block)
+    uri = args[0]
+
+    payload = {
+      uri: uri
+    }
+    ActiveSupport::Notifications.instrument(DownloadEventName, payload) do
       super *args, &block
     end
   end
